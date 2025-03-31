72e6ea7d5877dabc8d641af603acc88305667adf
DoRin1025 <lupu1025@gmail.com>
2025-03-31 21:39:11 +0300

    Day2

---
+++ Day2.py
@@ -0,0 +1,25 @@
+print("Hello"[1])
+
+print(len("Hello"))
+
+#print(type("Hello"))
+#print(type(123))
+
+#print("Number of latter in your name " + str(len(input("What is your name? "))))
+
+#Mathematical Operations
+
+print(45 +40)
+print(85 - 23)
+print(5*2)
+print(7/3)
+
+# Returneaza doar partea intreaga
+print(7//3)
+
+#Ridicarea la putere
+print(2**2)
+
+
+
+print(3/3*3+3-3)
