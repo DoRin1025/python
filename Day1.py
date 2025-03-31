ff39185702a0969ca7f75f07eb6da5b69af1ff5a
root <root@localhost.localdomain>
2025-03-31 20:46:22 +0300

    first commit

---
+++ Day1.py
@@ -0,0 +1,15 @@
+#print ("Hello world!")
+
+
+#iThis line of code use input like input function
+#print("Hello " + input("Waht id your name?"))
+
+#Variables
+
+#name = "Jack"
+#print(name)
+
+cityName = input("What is your city name? ")
+petName = input("What is your pet name? ")
+print("Your band name is " + cityName + " " + petName)
+
