7362f6d8411b497b385258f063e0ed3abc07a324
DoRin1025 <lupu1025@gmail.com>
2025-04-15 20:03:37 +0300

    Day3

---
+++ Day3/if_else.py
@@ -0,0 +1,6 @@
+
+height = int(input("Waht is your height in cm? "))
+if height >= 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry you have to grow taller bedore yum can ride ")
